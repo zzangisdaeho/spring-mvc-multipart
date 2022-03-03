36711a57ca5755e3f79f62b6dca20c7bf48707b4
DaeHo Kim <zzangisdaeho@naver.com>
2022-03-04 01:45:48 +0900

    first commit

---
+++ src/main/java/hello/upload/SpringMvcMultipartApplication.java
@@ -0,0 +1,13 @@
+package hello.upload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcMultipartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcMultipartApplication.class, args);
+    }
+
+}
